e3d993434cb5b38e5b9da22aa2aa3ba83650169b
Emilio Castro <emilio.castro@mercadolibre.com>
2020-10-12 12:55:33 -0300

    Adds download router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const uploadRouter = require('./routes/upload')
+const downloadRouter = require('./routes/download')
 
 const port = process.env.PORT || 5000
 
@@ -11,6 +13,8 @@ app.use(cors())
 
 // Routes
 app.get('/', (req, res) => res.send('Home cloud API'))
+app.use('/upload', uploadRouter)
+app.use('/download', downloadRouter)
 
 // Server
 app.listen(port, () => console.log('Server running on port', port))
