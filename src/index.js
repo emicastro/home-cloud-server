08ce10609cf046a21cd75fbe83c0bf8f1be74907
Emilio Castro <emilio.castro@mercadolibre.com>
2020-10-12 13:07:13 -0300

    Adds content router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dirRouter = require('./routes/dir')
+const contentRouter = require('./routes/content')
 const uploadRouter = require('./routes/upload')
 const downloadRouter = require('./routes/download')
 
@@ -15,6 +16,7 @@ app.use(cors())
 // Routes
 app.get('/', (req, res) => res.send('Home cloud API'))
 app.use('/dir', dirRouter)
+app.use('/content', contentRouter)
 app.use('/upload', uploadRouter)
 app.use('/download', downloadRouter)
 
